ee4f2209feb71f5683ad86b303abd1832bff0f2d
Khushi <dpskhu13108@gmail.com>
2021-01-23 20:01:34 +0530

    new commits

---
+++ app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickbtn(View view){
+        Log.i("this", "clickbtn: This is a message ");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
